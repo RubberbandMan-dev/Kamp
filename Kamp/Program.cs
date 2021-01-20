a061eeb822f323b45aedd1d89857a3b0b6d3057e
aberat <aberat@DESKTOP-6M0IKMP>
2021-01-20 23:50:33 +0300

    20.01.2021 ders sonu

--- Kamp/Program.cs
+++ Kamp/Program.cs
@@ -4,9 +4,10 @@ namespace Kamp
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+
         }
     }
 }
